7412e40a4b68f84c143d03afae97d8993ea6241c
Egor Minyazev <minyazev.egor050@gmail.com>
2026-09-20 18:01:24 +0300

    Complete homework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut elements = String::new();
+    
+    io::stdin()
+        .read_line(&mut elements)
+        .expect("Failed to read line");
+    
+    let mut parts = elements.split_whitespace();
+
+    let a: i32 = parts
+        .next()
+        .expect("Нет числа")
+        .parse()
+        .expect("Не число");
+
+    let b: i32 = parts
+        .next()
+        .expect("Нет числа")
+        .parse()
+        .expect("Не число");
+    p
+    rintln!("{}", a+b);
 }
