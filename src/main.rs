38d436efdba0113994024654639ca0fe39746946
Egor Minyazev <minyazev.egor050@gmail.com>
2026-09-20 18:13:35 +0300

    switch to i64 and read_to_string for input

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ fn main() {
     let mut elements = String::new();
     
     io::stdin()
-        .read_line(&mut elements)
+        .read_to_string(&mut elements)
         .expect("Failed to read line");
     
     let mut parts = elements.split_whitespace();
 
-    let a: i32 = parts
+    let a: i64 = parts
         .next()
         .expect("Нет числа")
         .parse()
         .expect("Не число");
 
-    let b: i32 = parts
+    let b: i64 = parts
         .next()
         .expect("Нет числа")
         .parse()
