bfb226eab758e15dabdb83e6cc2af659b7e7e648
Egor Minyazev <minyazev.egor050@gmail.com>
2026-09-20 18:18:53 +0300

    format fmt

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,16 @@ use std::io;
 
 fn main() {
     let mut elements = String::new();
-    
+
     io::stdin()
         .read_to_string(&mut elements)
         .expect("Failed to read line");
-    
+
     let mut parts = elements.split_whitespace();
 
-    let a: i64 = parts
-        .next()
-        .expect("Нет числа")
-        .parse()
-        .expect("Не число");
+    let a: i64 = parts.next().expect("Нет числа").parse().expect("Не число");
 
-    let b: i64 = parts
-        .next()
-        .expect("Нет числа")
-        .parse()
-        .expect("Не число");
-    
-    println!("{}", a+b);
+    let b: i64 = parts.next().expect("Нет числа").parse().expect("Не число");
+
+    println!("{}", a + b);
 }
